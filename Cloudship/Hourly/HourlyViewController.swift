7b7846bbc739f8ef413beaa3af6afa7368c70c03
Scott Kriss <scottkriss@gmail.com>
2018-01-30 20:26:56 -0800

    initial setup

---
+++ Cloudship/Hourly/HourlyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HourlyViewController.swift
+//  Cloudship
+//
+//  Created by Scott Kriss on 1/30/18.
+//  Copyright © 2018 Scott Kriss. All rights reserved.
+//
+
+import UIKit
+
+class HourlyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
