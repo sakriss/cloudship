97ac0c06923fa2c221a0d8bb1f47ef7324ccf00c
Scott Kriss <scottkriss@gmail.com>
2018-05-30 17:52:45 -0700

    Added minutely viewcontroller...not sure if I'll be using that

---
+++ Cloudship/Minutely/MinutelyTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MinutelyTableViewCell.swift
+//  Cloudship
+//
+//  Created by Scott Kriss on 5/30/18.
+//  Copyright © 2018 Scott Kriss. All rights reserved.
+//
+
+import UIKit
+
+class MinutelyTableViewCell: UITableViewCell {
+
+
+}
+
--- Cloudship/Minutely/MinutelyViewController.swift
+++ Cloudship/Minutely/MinutelyViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class MinutelyViewController: UIViewController {
     
+    @IBOutlet weak var minutelyTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        self.minutelyTableView.rowHeight = UITableViewAutomaticDimension
+        self.minutelyTableView.estimatedRowHeight = 80
+    }
+    
+
+}
+
+extension MinutelyViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (WeatherController.shared.weather?.minutely?.data?.count)!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MinutelyTableViewCell", for: indexPath) as? MinutelyTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let dataPoint = WeatherController.shared.weather
+        
+        
+        return cell
     }
     
 
