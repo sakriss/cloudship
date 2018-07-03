392877c2c88e5d351b32209dddebfdb8a7407856
Scott Kriss <scottkriss@gmail.com>
2018-07-03 15:44:12 -0700

    Added info view controller and powered by DarkSky button. Need to fill in the rest of this later

---
+++ Settings/InfoViewController.swift
@@ -0,0 +1,41 @@
+//
+//  InfoViewController.swift
+//  Cloudship
+//
+//  Created by Scott Kriss on 7/3/18.
+//  Copyright © 2018 Scott Kriss. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    @IBAction func poweredByDarkSkyLinkButton(_ sender: UIButton) {
+        if let url = NSURL(string: "https://darksky.net/poweredby/"){
+            UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+        }
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
